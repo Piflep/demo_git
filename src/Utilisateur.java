c706caa98ee56fa51a7996c02e0b3d050a116a8e
pif leprovost <pf.leprovost21310@orange.fr>
2020-07-02 11:29:16 +0200

    ajout des props de la classe utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,4 +1,18 @@
 
+/**
+ * 
+ * @author pifle
+ *
+ */
 public class Utilisateur {
-
-}
+	
+	/*_____________________ props _____________________*/
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	/*_____________________ props _____________________*/
+	
+	/*_____________________ props _____________________*/
+	
+}//end class
